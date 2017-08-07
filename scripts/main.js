b9b179682056f7235c8e3c022b66605d939ed3b5
Kelly Toomes <kelly@Kellys-MacBook.local>
2017-08-07 04:36:54 -0400

    finished

--- scripts/main.js
+++ scripts/main.js
@@ -9,40 +9,56 @@
    write your logic.
 */
 
-
+//define aArray outside of for loop
 
 function handValue (hand) {
-  let K = 10;
-  let Q = 10;
-  let J = 10;
-  let A = 11 || 1;
 
-  for(let i = 0; i < hand.length; i++){
-      if(hand[i] != A){
-      let sumHand = hand.reduce(function(a, b){return a + b;});
-      console.log(sumHand);
-  }
-  else{
-    let aArray = [];
-    hand.pop(hand[i]);
-    aArray.push(hand[i]);
-    for(let i = 0; i < hand.length; i++){
-      let sumHand = hand[i];
-      sumHand += hand[i+1];
-    }
-      if(sumHand > 10){
-        A = 1;
-        let sumAArray = aArray[i];
-        sumAArray += aArray[i+1];
-
-      }
-      else{
-        A = 11;
-        let sumAArray = aArray[i];
-      }
-      handValue = sumAArray + sumHand;
-  }
+  var aceHighArray = [0];
+  var jkqArray = [0];
+  var aceLowArray = [0];
+  var numArray = [0];
+   for(let i = 0; i < hand.length; i++){
+     if(hand[i] === 'K'){
+      jkqArray.push(10);
+     }
+     else if(hand[i] === 'Q'){
+       jkqArray.push(10);
+     }
+     else if(hand[i] === 'J'){
+      jkqArray.push(10);
+     }
+     else if(hand[i] === 'A'){
+     aceHighArray.push(1);
+     aceLowArray.push(11);
+     }
+     else{
+       numArray.push(hand[i]);
+     }
 }
+  var newJKQArray = jkqArray.map(Number);
+  var newAceHighArray = aceHighArray.map(Number);
+  var newAceLowArray = aceLowArray.map(Number);
+  var newNumArray = numArray.map(Number);
+  var handNum = newNumArray.reduce(function (a, b){
+    return a + b;} )
+    console.log(handNum);
+  var handFace = newJKQArray.reduce(function (a, b){
+    return a + b;} )
+  var numFace = handNum + handFace;
+  console.log( numFace);
+  var aceLow = newAceLowArray.reduce(function (a, b){
+    return a + b;})
+    console.log(aceLow);
+  var aceHigh = newAceHighArray.reduce(function (a, b){
+      return a + b;})
+  if(numFace <= 10){
+    var handValue = numFace + aceLow;
+    console.log('low' , handValue);
+  }
+  else {
+    var handValue = numFace + aceHigh;
+    console.log('high', handValue);
+  }
   return handValue;
 }
 
